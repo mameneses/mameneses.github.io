a4bd47e51469a293d2a302611c83ab910c62aaf1
Matias Meneses <matiasameneses@gmail.com>
2016-03-27 21:18:37 -0700

    updates

--- js/my_site.js
+++ js/my_site.js
@@ -1,5 +1,110 @@
-$(document).ready(function($){
 
+function showText() {
+
+}
+
+function bubble () {
+  var bubbleChart = new d3.svg.BubbleChart({
+    supportResponsive: true,
+    //container: => use @default
+    size: 600,
+    //viewBoxSize: => use @default
+    innerRadius: 600 / 3.5,
+    //outerRadius: => use @default
+    radiusMin: 50,
+    //radiusMax: use @default
+    //intersectDelta: use @default
+    //intersectInc: use @default
+    //circleColor: use @default
+    data: {
+      items: [
+        {text: "JavaScript", count: "93", color:"#FAFAFA"},
+        {text: "Ruby", count: "71",},
+        {text: "Rails", count: "65"},
+        {text: "GoLang", count: "32"},
+        {text: "React", count: "80"},
+        {text: "Redux", count: "72"},
+        {text: "Angular", count: "65"},
+        {text: "MongoDB", count: "85"},
+        {text: "Agile Development", count: "85"},
+        {text: "Lean Methodology", count: "79"}
+
+      ],
+      eval: function (item) {return item.count;},
+      classed: function (item) {return item.text.split(" ").join("");}
+    },
+    plugins: [
+      {
+        name: "central-click",
+        options: {
+          text: "(See more detail)",
+          style: {
+            "font-size": "12px",
+            "font-style": "italic",
+            "font-family": "Source Sans Pro, sans-serif",
+            //"font-weight": "700",
+            "text-anchor": "middle",
+            "fill": "white"
+          },
+          attr: {dy: "65px"},
+          centralClick: function() {
+            console.log(this)
+            alert("Here is more details!!");
+          }
+        }
+      },
+      {
+        name: "lines",
+        options: {
+          format: [
+            {// Line #0
+              textField: "count",
+              classed: {count: true},
+              style: {
+                "font-size": "28px",
+                "font-family": "Source Sans Pro, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "0px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            },
+            {// Line #1
+              textField: "text",
+              classed: {text: true},
+              style: {
+                "font-size": "14px",
+                "font-family": "Source Sans Pro, sans-serif",
+                "text-anchor": "middle",
+                fill: "white"
+              },
+              attr: {
+                dy: "20px",
+                x: function (d) {return d.cx;},
+                y: function (d) {return d.cy;}
+              }
+            }
+          ],
+          centralFormat: [
+            {// Line #0
+              style: {"font-size": "50px"},
+              attr: {}
+            },
+            {// Line #1
+              style: {"font-size": "30px"},
+              attr: {dy: "40px"}
+            }
+          ]
+        }
+      }]
+  });
+};
+
+
+$(document).ready(function($){
  $("#home-button").click(function() {
     $('html,body').animate({
         scrollTop: $("#home").offset().top},
@@ -64,8 +169,8 @@ var prev, next;
 var currentPostion = 0;
 var currentImage = 0;
 
-
 function init(){
+  bubble()
   ul = document.getElementById('image_slider');
   li_items = ul.children;
   imageNumber = li_items.length;
@@ -80,6 +185,9 @@ function init(){
   prev.onclick = function(){ onClickPrev();};
   next.onclick = function(){ onClickNext();};
 }
+
+
+
 
 function animate(opts){
   var start = new Date;
