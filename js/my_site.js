401e80f34068c8b16e6b429bb3ba0c02d5971de2
Matias Meneses <matiasameneses@gmail.com>
2019-11-15 16:36:53 -0800

    updates title and about me

--- js/my_site.js
+++ js/my_site.js
@@ -1,109 +1,4 @@
 
-function showText() {
-
-}
-
-function bubble () {
-  var bubbleChart = new d3.svg.BubbleChart({
-    supportResponsive: true,
-    //container: => use @default
-    size: 600,
-    //viewBoxSize: => use @default
-    innerRadius: 600 / 3.5,
-    //outerRadius: => use @default
-    radiusMin: 50,
-    //radiusMax: use @default
-    //intersectDelta: use @default
-    //intersectInc: use @default
-    //circleColor: use @default
-    data: {
-      items: [
-        {text: "JavaScript", count: "93", color:"#FAFAFA"},
-        {text: "Ruby", count: "71",},
-        {text: "Rails", count: "65"},
-        {text: "GoLang", count: "32"},
-        {text: "React", count: "80"},
-        {text: "Redux", count: "72"},
-        {text: "Angular", count: "65"},
-        {text: "MongoDB", count: "85"},
-        {text: "Agile Development", count: "85"},
-        {text: "Lean Methodology", count: "79"}
-
-      ],
-      eval: function (item) {return item.count;},
-      classed: function (item) {return item.text.split(" ").join("");}
-    },
-    plugins: [
-      {
-        name: "central-click",
-        options: {
-          text: "(See more detail)",
-          style: {
-            "font-size": "12px",
-            "font-style": "italic",
-            "font-family": "Source Sans Pro, sans-serif",
-            //"font-weight": "700",
-            "text-anchor": "middle",
-            "fill": "white"
-          },
-          attr: {dy: "65px"},
-          centralClick: function() {
-            console.log(this)
-            alert("Here is more details!!");
-          }
-        }
-      },
-      {
-        name: "lines",
-        options: {
-          format: [
-            {// Line #0
-              textField: "count",
-              classed: {count: true},
-              style: {
-                "font-size": "28px",
-                "font-family": "Source Sans Pro, sans-serif",
-                "text-anchor": "middle",
-                fill: "white"
-              },
-              attr: {
-                dy: "0px",
-                x: function (d) {return d.cx;},
-                y: function (d) {return d.cy;}
-              }
-            },
-            {// Line #1
-              textField: "text",
-              classed: {text: true},
-              style: {
-                "font-size": "14px",
-                "font-family": "Source Sans Pro, sans-serif",
-                "text-anchor": "middle",
-                fill: "white"
-              },
-              attr: {
-                dy: "20px",
-                x: function (d) {return d.cx;},
-                y: function (d) {return d.cy;}
-              }
-            }
-          ],
-          centralFormat: [
-            {// Line #0
-              style: {"font-size": "50px"},
-              attr: {}
-            },
-            {// Line #1
-              style: {"font-size": "30px"},
-              attr: {dy: "40px"}
-            }
-          ]
-        }
-      }]
-  });
-};
-
-
 $(document).ready(function($){
  $("#home-button").click(function() {
     $('html,body').animate({
@@ -123,7 +18,7 @@ $(document).ready(function($){
         'slow');
   });
 
-  var $window = $(window); 
+  var $window = $(window);
 
   $('section[data-type="background"]').each(function(){
   var $bgobj = $(this);
@@ -144,7 +39,7 @@ $(document).ready(function($){
     {
       $("#title").text("About Me")
     } else {
-      $("#title").text("Projects")
+      $("#title").text("Work")
     }
     if (iCurScrollPos >=1000 ) {
       $(".about-title").css("display","none")
@@ -153,9 +48,9 @@ $(document).ready(function($){
     }
 
     if (iCurScrollPos >=2000) {
-      $(".project-title").css("display","none")
+      $(".work-title").css("display","none")
     } else {
-      $(".project-title").css("display","inline")
+      $(".work-title").css("display","inline")
     }
  });
 
@@ -170,7 +65,6 @@ var currentPostion = 0;
 var currentImage = 0;
 
 function init(){
-  bubble()
   ul = document.getElementById('image_slider');
   li_items = ul.children;
   imageNumber = li_items.length;
@@ -220,7 +114,7 @@ function slideTo(imageToGo){
     step:function(delta){
       ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
     },
-    callback:function(){currentImage = imageToGo;}  
+    callback:function(){currentImage = imageToGo;}
   };
   animate(opts);
 }
@@ -231,12 +125,12 @@ function onClickPrev(){
     $("[id='" + currentImage.toString() +"']").css("display","none")
     $("[id='" + (imageNumber - 1).toString() +"']").css("display","inline")
 
-  }     
+  }
   else{
     slideTo(currentImage - 1);
     $("[id='" + currentImage.toString() +"']").css("display","none")
     $("[id='" + (currentImage - 1).toString() +"']").css("display","inline")
-  }   
+  }
 }
 
 function onClickNext(){
@@ -244,12 +138,12 @@ function onClickNext(){
     slideTo(0);
     $("[id='" + currentImage.toString() +"']").css("display","none")
     $("[id='0']").css("display","inline")
-  }   
+  }
   else{
     slideTo(currentImage + 1);
     $("[id='" + currentImage.toString() +"']").css("display","none")
     $("[id='" + (currentImage + 1).toString() +"']").css("display","inline")
-  }   
+  }
 }
 
 window.onload = init;
